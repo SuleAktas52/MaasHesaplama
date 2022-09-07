6380fcb78ea1a300995900868a21fefce0d398ca
Şule Aktaş <suleaktasgs@gmail.com>
2022-09-07 15:36:19 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package MaasHesaplayicisi;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee Sule= new Employee("Sule", 9000, 45, 2020);
+        Sule.print();
+    }
+}
